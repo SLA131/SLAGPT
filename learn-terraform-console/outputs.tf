8b094228c031f1d5009cacdee94ce46be6f052dc
SLA131 <129662780+SLA131@users.noreply.github.com>
2023-04-22 02:49:45 +0800

    Delete learn-terraform-console directory

--- learn-terraform-console/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# Output values
-
-output "s3_bucket_name" {
-  description = "Name of our S3 bucket."
-  value       = aws_s3_bucket.data.bucket
-}
